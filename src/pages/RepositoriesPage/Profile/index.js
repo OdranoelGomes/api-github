09582e60d8537ace5dbc52ff480d6e7104f82959
Leonardo G. da Silva <102685888+OdranoelGomes@users.noreply.github.com>
2022-10-24 11:20:49 -0300

    Update - Repositories
    Filter - Parte1 Finalizado!

--- src/pages/RepositoriesPage/Profile/index.js
+++ src/pages/RepositoriesPage/Profile/index.js
@@ -1,33 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
 
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
-const Profile = () => (
+const Profile = ({ user }) => (
   <Container>
     <Header>
-      <Avatar src="https://avatars.githubusercontent.com/u/102685888?v=4" />
-      <Login>odranoelgomes</Login>
-      <Name>Leonardo Gomes</Name>
+      <Avatar src={user.avatar_url} />
+      <Login>{user.login}</Login>
+      <Name>{user.name}</Name>
     </Header>
     <Inner>
       <Data>
         <MdGroup size={20} />
-        30&nbsp;<i>seguidores</i>&nbsp; &middot; 10&nbsp;<i>seguindo</i>
+        {user.following}&nbsp;<i>seguidores</i>&nbsp; &middot;
+        {user.followers}&nbsp;<i>seguindo</i>
       </Data>
-      <Data>
-        <MdWork size={20} /> Dev Detalhe
-      </Data>
-      <Data>
-        <MdLocationCity size={20} />
-        Meier Rio de Janeiro</Data>
-      <Data>
-        <MdLink size={20} />
-        <a href="http://www.detalherj.com.br">detalherj.com.br</a>
-      </Data>
+      {user.company && (
+        <Data>
+          <MdWork size={20} /> {user.company}
+        </Data>
+      )}
+      {user.location && (
+        <Data>
+          <MdLocationCity size={20} /> {user.location}
+        </Data>
+      )}
+      {user.blog && (
+        <Data>
+          <MdLink size={20} />
+          <a href={`\\${user.blog}`}>{user.blog}</a>
+        </Data>
+      )}
     </Inner>
   </Container>
 );
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
 
 export default Profile;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -6,16 +6,78 @@ import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
-const RepositoriesPage = () => (
-  <Container>
-    <Sidebar>
-      <Profile />
-      <Filter />
-    </Sidebar>
-    <Main>
-      <Repositories />
-    </Main>
-  </Container>
-);
+const RepositoriesPage = () => {
+  const user = {
+    login: 'OdranoelGomes',
+    name: 'Leonardo G da Silva',
+    avatar_url: 'https://avatars.githubusercontent.com/u/102685888?v=4',
+    followers: 0,
+    following: 0,
+    company: null,
+    blog: 'https://www.detalherj.com.br',
+    location: 'Coelho da Rocha - Brasil',
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const repositories = [
+    {
+      name: 'Repo 1',
+      description: 'Descrição',
+      html_url: 'https://www.detalherj.com.br',
+      language: 'JavaScript',
+    },
+    {
+      name: 'Repo 2',
+      description: 'Descrição',
+      html_url: 'https://www.detalherj.com.br',
+      language: 'JavaScript',
+    },
+    {
+      name: 'Repo 3',
+      description: 'Descrição',
+      html_url: 'https://www.detalherj.com.br',
+      language: 'PHP',
+    },
+    {
+      name: 'Repo 4',
+      description: 'Descrição',
+      html_url: 'https://www.detalherj.com.br',
+      language: 'Ruby',
+    },
+    {
+      name: 'Repo 5',
+      description: 'Descrição',
+      html_url: 'https://www.detalherj.com.br',
+      language: 'Java',
+    },
+    {
+      name: 'Repo 6',
+      description: 'Descrição',
+      html_url: 'https://www.detalherj.com.br',
+      language: 'TypeScript',
+    },
+  ];
+
+  // Calculo dos Filters
+
+  const languages = [
+    { name: 'JavaScript', count: 3, color: '#f1c40f' },
+    { name: 'Shell', count: 2, color: '#95a5a6' },
+    { name: 'PHP', count: 2, color: '#3498db' },
+    { name: 'Ruby', count: 1, color: '#e74c3c' },
+  ];
+
+  return (
+    <Container>
+      <Sidebar>
+        <Profile user={user} />
+        <Filter languages={languages} />
+      </Sidebar>
+      <Main>
+        <Repositories />
+      </Main>
+    </Container>
+  );
+};
 
 export default RepositoriesPage;
