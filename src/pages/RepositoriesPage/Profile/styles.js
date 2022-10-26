d328250607480e47a92fdb550af6f0cd6292d403
Leonardo G. da Silva <102685888+OdranoelGomes@users.noreply.github.com>
2022-10-26 11:04:22 -0300

    Update Projeto
    Finalizado!

--- src/pages/RepositoriesPage/Profile/styles.js
+++ src/pages/RepositoriesPage/Profile/styles.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
-
 export const Container = styled.div`
   padding: 1rem;
 `;
 
-export const Header = styled.div `
+export const Header = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1rem 0;
